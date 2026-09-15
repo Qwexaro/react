6784cb6cb9fcf3432155505b7394cfd1eddb72c4
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-09-15 15:19:43 +0300

    upgraded style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,34 @@
 import Actions from './Actions'
 
 let Post = ({ author, title, text }) =>
-    <article className='post'>
-        <h2>{title}</h2>
-        
-        <p className='post-text'>{text}</p>
-        
-        <p className='post-author'>Автор: {author}</p>
+    <div>
+        <article className='post'>
+            <h2>Цитаты великих людей</h2>
 
-        <Actions />
-    </article>;
+            <p className='post-text'>Какая цитата?</p>
+
+            <p className='post-author'>Автор: Maxim</p>
+
+            <Actions />
+        </article>
+        <article className='post'>
+            <h2>Цитаты великих людей</h2>
+
+            <p className='post-text'>О Отделе неизбежного</p>
+
+            <p className='post-author'>Автор: Kirill</p>
+
+            <Actions />
+        </article>
+        <article className='post'>
+            <h2>Цитаты великих людей</h2>
+
+            <p className='post-text'>О квантовом бессмертии</p>
+
+            <p className='post-author'>Автор: Sokrat</p>
+
+            <Actions />
+        </article>
+    </div>;
 
 export default Post;
