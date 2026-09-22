869739d80f942116a12fcf6c274d84c7d583050b
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-09-22 15:52:07 +0300

    updated ProfileCard.jsx

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,25 +1,94 @@
+import { useState } from 'react';
 import Post from './Post'
 
-let ProfileCard = () =>
-    <section className='profile-card'>
-        <div className='profile'>
-            <div className='avatar'>
-                avatar
-            </div>
-            <div className='profile-info'>
-                <h2>Name</h2>
-                <p>@nick</p>
-            </div>
-        </div>
 
-        <Post author={"Viktor"} title={"Shadow"} text={"Lorem"} />
-        <Post author={"Alex"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Sam"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Sokrat"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Maxim"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Dima"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Misha"} title={"Shadow"} text={"Any text"} />
-        <Post author={"Semen"} title={"Shadow"} text={"Any text"} />
-    </section>;
+let addPost = event => {
+
+    event.preventDefault();
+
+    const newPost = {
+
+        id: Date.now(),
+
+        title: title,
+
+        text: text,
+
+        author: "Viktor"
+
+    }
+
+    setPosts(...postsData, newPost);
+
+    setTitle("");
+
+    setText("");
+
+}
+
+
+let ProfileCard = () => {
+
+    const [posts, setPosts] = useState([
+
+        { id: 1, author: 'Viktor', title: 'Study react for frontend', text: 'Any text' },
+
+        { id: 2, author: 'Viktor', title: 'Study react for frontend', text: 'Any text' },
+
+        { id: 3, author: 'Viktor', title: 'Study react for frontend', text: 'Any text' },
+
+        { id: 4, author: 'Viktor', title: 'Study react for frontend', text: 'Any text' }
+
+    ])
+
+
+    const [title, setTitle] = useState('');
+
+    const [text, setText] = useState("");
+
+
+    return (
+        < section className='profile-card' >
+            <div className='profile'>
+                <div className='avatar'>
+                    avatar
+                </div>
+                <div className='profile-info'>
+                    <h2>Name</h2>
+                    <p>@nick</p>
+                </div>
+            </div>
+
+
+            <form className="post-form" onSubmit={ addPost }>
+                <input
+                    type="text"
+
+                    placeholder='Заголовок'
+
+                    value={title}
+
+                    onChange={(event) => setTitle(event.target.value)}
+                />
+            </form>
+
+
+            {
+                posts.map(post => (
+                    <Post
+                        key={post.id}
+
+                        author={post.author}
+
+                        title={post.title}
+
+                        text={post.text}
+                    />
+                ))
+            }
+
+        </section >);
+}
+
 
 export default ProfileCard;
