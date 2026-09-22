c1916a2718c90dc66ec52e6cdd153d8a6c33000a
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-09-22 14:40:34 +0300

    Remake profile card
    - modified:   Post.jsx
    - modified:   ProfileCard.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,32 +3,15 @@ import Actions from './Actions'
 let Post = ({ author, title, text }) =>
     <div>
         <article className='post'>
-            <h2>Цитаты великих людей</h2>
+            <h2>{ title }</h2>
 
-            <p className='post-text'>Какая цитата?</p>
+            <p className='post-text'>{ text }</p>
 
-            <p className='post-author'>Автор: Maxim</p>
-
-            <Actions />
-        </article>
-        <article className='post'>
-            <h2>Цитаты великих людей</h2>
-
-            <p className='post-text'>О Отделе неизбежного</p>
-
-            <p className='post-author'>Автор: Kirill</p>
+            <p className='post-author'>Author: { author }</p>
 
             <Actions />
         </article>
-        <article className='post'>
-            <h2>Цитаты великих людей</h2>
-
-            <p className='post-text'>О квантовом бессмертии</p>
-
-            <p className='post-author'>Автор: Sokrat</p>
-
-            <Actions />
-        </article>
+        
     </div>;
 
 export default Post;
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -11,7 +11,15 @@ let ProfileCard = () =>
                 <p>@nick</p>
             </div>
         </div>
-        <Post />
+
+        <Post author={"Viktor"} title={"Shadow"} text={"Lorem"} />
+        <Post author={"Alex"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Sam"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Sokrat"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Maxim"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Dima"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Misha"} title={"Shadow"} text={"Any text"} />
+        <Post author={"Semen"} title={"Shadow"} text={"Any text"} />
     </section>;
 
 export default ProfileCard;
